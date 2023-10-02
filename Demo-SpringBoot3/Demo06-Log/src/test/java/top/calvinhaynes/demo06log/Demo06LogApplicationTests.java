80a2c70a73ba114c06d4bc9a72ea5710dfb5b967
Calvin Haynes <chx1006488386@163.com>
2023-10-02 22:55:22 +0800

    update

---
+++ Demo-SpringBoot3/Demo06-Log/src/test/java/top/calvinhaynes/demo06log/Demo06LogApplicationTests.java
@@ -0,0 +1,13 @@
+package top.calvinhaynes.demo06log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo06LogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
